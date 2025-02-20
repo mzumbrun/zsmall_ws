8e00e7d692f7bab2070bca8851375713b59a609c
mzumbrun <m.l.zumbrunnen@gmail.com>
2025-02-19 19:44:58 -0600

    fixes for urdf experiment

--- src/zsmall_description/launch/show_smallbot.launch.py
+++ src/zsmall_description/launch/show_smallbot.launch.py
@@ -1,6 +1,6 @@
 import os
 from ament_index_python.packages import get_package_share_directory
-
+import xacro
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration
@@ -12,18 +12,20 @@ from launch_ros.parameter_descriptions import ParameterValue
 def generate_launch_description():
     zsmall_description_dir = get_package_share_directory("zsmall_description")
 
-    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
-                                        zsmall_description_dir, "urdf", "smallbot.urdf.xacro"
-                                        ),
-                                      description="Absolute path to robot urdf file")
-
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
-                                       value_type=str)
-
+    # Process the URDF file
+    pkg_path = os.path.join(get_package_share_directory('zsmall_description'))
+    xacro_file = os.path.join(pkg_path,'urdf','smallbot.urdf.xacro')
+    doc = xacro.process_file(xacro_file, mappings={'is_sim' : 'false', 'use_ros2_control' : 'true', 'use_arm' : 'false' })
+    robot_description = doc.toprettyxml(indent='  ')
+    
+    # Create a robot_state_publisher node
+    params = {'robot_description': robot_description, 'use_sim_time': True}
+    
     robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_description}]
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
     )
 
     joint_state_publisher_gui_node = Node(
@@ -40,7 +42,6 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        model_arg,
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
         rviz_node
